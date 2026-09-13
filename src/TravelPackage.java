4172eb0b84b31b6ad626d3c58394bf74bb0a950a
Zhibek <zibekkypsak123@gmail.com>
2026-09-13 14:44:52 +0300

    Add TravelPackage product class

---
+++ src/TravelPackage.java
@@ -0,0 +1,44 @@
+public class TravelPackage {
+
+    private String destination;
+    private String hotel;
+    private String transport;
+    private String meals;
+    private String activities;
+    private String insurance;
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public void setHotel(String hotel) {
+        this.hotel = hotel;
+    }
+
+    public void setTransport(String transport) {
+        this.transport = transport;
+    }
+
+    public void setMeals(String meals) {
+        this.meals = meals;
+    }
+
+    public void setActivities(String activities) {
+        this.activities = activities;
+    }
+
+    public void setInsurance(String insurance) {
+        this.insurance = insurance;
+    }
+
+    @Override
+    public String toString() {
+        return "Travel Package:" +
+                "\nDestination: " + destination +
+                "\nHotel: " + hotel +
+                "\nTransport: " + transport +
+                "\nMeals: " + meals +
+                "\nActivities: " + activities +
+                "\nInsurance: " + insurance;
+    }
+}

c5be6b15608f6cbf2848fca018d1e5dad6ffbecb
Zhibek <zibekkypsak123@gmail.com>
2026-09-13 16:27:07 +0300

    Add LuxuryTravelBuilder implementation

---
+++ src/LuxuryTravelBuilder.java
@@ -0,0 +1,42 @@
+public class LuxuryTravelBuilder implements TravelBuilder {
+
+    private TravelPackage travelPackage;
+
+    public LuxuryTravelBuilder() {
+        this.travelPackage = new TravelPackage();
+    }
+    @Override
+    public TravelBuilder setDestination(String destination) {
+        travelPackage.setDestination(destination);
+        return this;
+    }
+    @Override
+    public TravelBuilder setHotel(String hotel) {
+        travelPackage.setHotel(hotel);
+        return this;
+    }
+    @Override
+    public TravelBuilder setTransport(String transport) {
+        travelPackage.setTransport(transport);
+        return this;
+    }
+    @Override
+    public TravelBuilder setMeals(String meals) {
+        travelPackage.setMeals(meals);
+        return this;
+    }
+    @Override
+    public TravelBuilder setActivities(String activities) {
+        travelPackage.setActivities(activities);
+        return this;
+    }
+    @Override
+    public TravelBuilder setInsurance(String insurance) {
+        travelPackage.setInsurance(insurance);
+        return this;
+    }
+    @Override
+    public TravelPackage build() {
+        return travelPackage;
+    }
+}
